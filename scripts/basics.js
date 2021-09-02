42e6b057989125482c54bbac251d2d013e020d56
onias17 <oniasnephiisrael@gmail.com>
2021-09-02 09:31:14 -0700

    js basics

---
+++ scripts/basics.js
@@ -0,0 +1,68 @@
+alert("hello world!")
+
+// this is a comment
+
+// ----- Basic Data Types
+// Integers
+10
+20.2
+-13.4
+
+// addition
+1 + 3 // 4
+
+// subtraction
+7 - 5 // 2
+
+// multiplication
+4 * 5 // 20
+
+// division
+10/2 // 5
+
+// exponents
+2 ** 4 // 16
+
+// modulo
+15 % 14 // 1 (returns the remainder)
+
+// Strings
+"hello world!"
+"10"
+
+// concatenation
+"Django" + " is super cool" // "Djaingo is super cool"
+
+// length
+"django".length // 6
+"x x".length // 3
+
+// new line and tabs
+"hello \n start a new line" // starts a new line
+"hello \t give me a tab" // makes a tab
+
+// indexing
+"hello"[4] // o
+"look at the dog"[12] // d
+
+// Booleans
+true 
+false
+
+// undefined and null
+undefined
+null
+
+// Variables
+let bankAccount = 100
+let deposit = 50
+
+let greeting = "Welcome back"
+let name = " Onias!"
+alert(greeting + name)
+
+// Conosle.log
+console.log("yo")
+
+// Prompt
+prompt("Enter your age:" )
