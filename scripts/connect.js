630060fc8e64183d474819d7960885e94783b451
onias17 <oniasnephiisrael@gmail.com>
2021-09-02 09:48:45 -0700

    js exercise

---
+++ scripts/connect.js
@@ -0,0 +1,4 @@
+alert("Welcome to your bank account")
+let deposit = prompt("How much do you want to deposit today?")
+alert("You've deposited: " + deposit)
+console.log("javascript")
---
+++ scripts/part3.js
@@ -0,0 +1,4 @@
+let lbs = prompt("enter weight in pounds here: ");
+let kg = lbs * .454;
+alert("Your weight is " + kg.toFixed(2) + "kg");
+console.log("Conversion Completed");
