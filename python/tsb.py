ca1bd9f7c0b39a76fd6b18c351a910ffd629aa8c
onias17 <oniasnephiisrael@gmail.com>
2021-09-03 15:24:03 -0700

    python tuples lists and booleans

---
+++ python/tsb.py
@@ -0,0 +1,20 @@
+# BOOLEANS
+True
+False
+0
+1
+
+# TUPLES cannot change
+t = (1,2,3)
+print(t[0])
+
+# SETS - unordered / cannot duplicate elements
+x = set()
+
+x.add(1)
+x.add(2)
+x.add(4)
+print(x)
+
+converted = set([1,1,1,1,2,2,2,2,3,3,3,3])
+print(converted)
