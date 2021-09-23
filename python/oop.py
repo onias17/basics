7988850b918f1d803804775a968015aca6681af1
onias17 <oniasnephiisrael@gmail.com>
2021-09-22 22:06:33 -0700

    python oop part 3

--- python/oop.py
+++ python/oop.py
@@ -34,6 +34,7 @@ print(mydog.species)
 print(type(mydog))
 
 odog = Dog('huskie', 'enoch')
+print(odog)
 print(odog.breed)
 print(odog.name)
 
@@ -79,8 +80,37 @@ class Dogg(Animal):
         Animal.__init__(self)
         print('dogg')
 
+    def bark(self):
+        print('woof')
+
+    def eat(self):
+        print('treat')
+
 mydogg = Dogg()
 mydogg.whoAmI()
 mydogg.eat()
 
 # Special Methods
+class Book():
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def __str__(self):
+        return 'Title: {}, Author: {}, Pages: {}'.format(self.title, self.author, self.pages)
+
+    def __len__(self):
+        return self.pages
+    
+    def __del__(self):
+        print('a book is destroyed')
+
+b = Book('Romans', 'Paul', 16)
+print(b)
+print(len(b))
+del b
+
+alist = [1,2,3]
+print(alist)
+
